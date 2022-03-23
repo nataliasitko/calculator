b8e3af99d797979c59e2a1dc56a3d59a59b89df6
Natalia Sitko~ <nataliasitko@interia.pl>
2022-03-23 21:14:37 +0100

    Update config

--- config.php
+++ config.php
@@ -1,13 +1,7 @@
 <?php
 
-define('_SERVER_NAME', 'localhost');
-define('_SERVER_URL', 'http://'._SERVER_NAME);
-define('_APP_ROOT', '/calculator');
-define('_APP_URL', _SERVER_URL._APP_ROOT);
+const _SERVER_NAME = 'localhost';
+const _SERVER_URL = 'http://' . _SERVER_NAME;
+const _APP_ROOT = '/calculator';
+const _APP_URL = _SERVER_URL . _APP_ROOT;
 define('_ROOT_PATH', dirname(__FILE__));
-
-function out($param){
-  if(isset($param)){
-      echo $param;
-  }  
-}
