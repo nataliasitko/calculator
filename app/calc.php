d01dd5bc615f7e3c0922bc27fcbc0ab6a28dfef6
Natalia Sitko~ <nataliasitko@interia.pl>
2022-03-23 06:53:00 +0100

    Update calc controller

--- app/calc.php
+++ app/calc.php
@@ -1,30 +1,31 @@
 <?php
 require_once dirname(__DIR__).'/config.php';
+require_once _ROOT_PATH.'lib/Smarty/Smarty.class.php';
 
 include _ROOT_PATH.'/app/security/check.php';
 
 function getParams(&$form){
-	$form['loan_amount'] = isset($_REQUEST['loan_amount']) ? $_REQUEST['loan_amount'] : null;
-	$form['intrest_rate'] = isset($_REQUEST['intrest_rate']) ? $_REQUEST['intrest_rate'] : null;
-	$form['num_of_months'] = isset($_REQUEST['num_of_months']) ? $_REQUEST['num_of_months'] : null;	
+	$form['loan_amount'] = $_REQUEST['loan_amount'] ?? null;
+	$form['interest_rate'] = $_REQUEST['interest_rate'] ?? null;
+	$form['num_of_months'] = $_REQUEST['num_of_months'] ?? null;
 }
 
-function validate(&$form,&$infos,&$msgs,&$hide_intro){
+function validate(&$form,&$infos,&$msgs,&$hide_intro): bool
+{
 
-	if ( ! (isset($form['loan_amount']) && isset($form['intrest_rate']) && isset($form['num_of_months']) ))	return false;	
+	if ( ! (isset($form['loan_amount']) && isset($form['interest_rate']) && isset($form['num_of_months']) ))	return false;
 	
 	$hide_intro = true;
 
 	$infos [] = 'Przekazano parametry.';
 
-	// sprawdzenie, czy potrzebne wartości zostały przekazane
 	if ( $form['loan_amount'] == "") $msgs [] = 'Nie podano liczby 1';
-	if ( $form['intrest_rate'] == "") $msgs [] = 'Nie podano liczby 2';
+	if ( $form['interest_rate'] == "") $msgs [] = 'Nie podano liczby 2';
         if ( $form['num_of_months'] == "") $msgs [] = 'Nie podano liczby 3';
 	
 	if ( count($msgs)==0 ) {
 		if (! is_numeric( $form['loan_amount'] )) $msgs [] = 'Pierwsza wartość nie jest liczbą';
-		if (! is_numeric( $form['intrest_rate'] )) $msgs [] = 'Druga wartość nie jest liczbą';
+		if (! is_numeric( $form['interest_rate'] )) $msgs [] = 'Druga wartość nie jest liczbą';
                 if (! is_numeric( $form['num_of_months'] )) $msgs [] = 'Trzecia wartość nie jest liczbą';
                 
         }
@@ -38,29 +39,38 @@ function process(&$form,&$infos,&$msgs,&$result){
 	
 	$form['loan_amount'] = floatval($form['loan_amount']);
         $form['num_of_months'] = intval($form['num_of_months']);
-        $form['intrest_rate'] = floatval($form['intrest_rate']);
+        $form['interest_rate'] = floatval($form['interest_rate']);
 
-        $result = $form['loan_amount']*($form['intrest_rate']/1200)*pow(1+$form['intrest_rate']/1200 ,$form['num_of_months'])
-        /(pow(1+$form['intrest_rate']/1200 ,$form['num_of_months'])-1);
+        $result = $form['loan_amount']*($form['interest_rate']/1200)*pow(1+$form['interest_rate']/1200 ,$form['num_of_months'])
+        /(pow(1+$form['interest_rate']/1200 ,$form['num_of_months'])-1);
 
         $result = round($result,2);
 }
-
 
 $form = null;
 $infos = array();
 $messages = array();
 $result = null;
 $hide_intro = false;
-	
+
 getParams($form);
 if ( validate($form,$infos,$messages,$hide_intro) ){
-	process($form,$infos,$messages,$result);
+    process($form,$infos,$messages,$result);
 }
 
-$page_title= 'Kalkulator kredytowy';
-$page_description = 'Podaj kwotę kredytu, oprocentowanie w skali roku oraz okres spłaty w miesiącach.';
-$page_header = 'Kalkulator kredytowy';
-$page_footer = 'projekt kalkulatora kredytowego';
+$smarty = new Smarty();
 
-include 'calc_view.php';
+$smarty->assign('app_url',_APP_URL);
+$smarty->assign('root_path',_ROOT_PATH);
+$smarty->assign('page_title','Kalkulator');
+$smarty->assign('page_description','Podaj kwotę kredytu, oprocentowanie w skali roku 
+                oraz okres spłaty liczony w miesiącach.');
+$smarty->assign('page_header','Kalkulator kredytowy');
+$smarty->assign('page_footer','Projekt kalkulatora kredytowego');
+
+$smarty->assign('form',$form);
+$smarty->assign('result',$result);
+$smarty->assign('messages',$messages);
+$smarty->assign('infos',$infos);
+
+$smarty->display(_ROOT_PATH.'/app/calc_view.tpl');
